30ff591a41cedc8350c63c88388cf95dd5d7ddb9
Simon Emmanuel Gutierrez Brida <simon.gutierrez.brida@gmail.com>
2017-02-21 16:53:02 -0300

    make changes to Smallest to test the pretty print option

--- test/arithmetic/Smallest.java
+++ test/arithmetic/Smallest.java
@@ -4,15 +4,46 @@ public class Smallest {
 
     public int smallest(int a, int b, int c, int d) {
         int smallest = 0;
-        if (a < b && a < c && a < d)
-                    smallest = a;
-        if (b < a && b < c && b < d)
+        if (a < b && a < c && a < d) {
+            smallest = a;
+            smallest = a;
+        }
+        else if (b < a && b < c && b < d)
                     smallest = b;
-        if (c < b && c < a && c < d)
+        else if (c < b && c < a && c < d) {
                     smallest = c;
-        if (d < b && d < c && d < a)
-                    smallest = d;
-        return smallest;
+                    smallest = a;
+        }
+        else if (d < b && d < c && d < a)
+                    smallest = d; //mutGenLimit 1
+        else {
+            smallest = a;
+            smallest = a;
+        }
+        if (a > 0) {
+        	a++;
+        	a++;
+        } else
+        	a--;
+        if (a < 0)
+        	a++;
+        else {
+        	a++;
+        	a++;
+        }
+        for (int i = 0; i < a; i++)
+        	System.out.println(i);
+        for (int i = 0; i < a; i++) {
+        	System.out.println("hola");
+        	System.out.println(i);
+        }
+        while (a > 0)
+        	a--;
+        while (b > 0) {
+        	a++;
+        	b++;
+        }
+        return smallest; //mutGenLimit 1
     } 
 
 }
