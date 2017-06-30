e903b1d499a7734ac0c041a19d94e0579a25d428
Simon Emmanuel Gutierrez Brida <simon.gutierrez.brida@gmail.com>
2017-06-30 17:03:46 -0300

    Made the class Serializable and improved the code that checked if a test failed because of timeout

--- src/mujava/app/TestResult.java
+++ src/mujava/app/TestResult.java
@@ -1,31 +1,44 @@
 package mujava.app;
 
+import java.io.Serializable;
 import java.util.List;
 
 import org.junit.runner.Result;
 import org.junit.runner.notification.Failure;
 
-public class TestResult {
+public class TestResult implements Serializable {
 
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = -3718457180972664524L;
 	private Result result;
 	private Class<?> testClassRunned;
-	private int totalTests = 0;
-	private int assertFailingTests = 0;
-	private int totalFailures = 0;
-	private int passingTests = 0;
-	private int timeoutFailures = 0;
-	private int exceptionFailures = 0;
-	//private int ignoredTests = 0;
+	private transient int totalTests = 0;
+	private transient int assertFailingTests = 0;
+	private transient int totalFailures = 0;
+	private transient int passingTests = 0;
+	private transient int timeoutFailures = 0;
+	private transient int exceptionFailures = 0;
 	
 	public TestResult(Result result, Class<?> testClassRunned) {
 		this.result = result;
 		this.testClassRunned = testClassRunned;
+		refresh();
+	}
+	
+	public void refresh() {
+		this.totalTests = 0;
+		this.assertFailingTests = 0;
+		this.totalFailures = 0;
+		this.passingTests = 0;
+		this.timeoutFailures = 0;
+		this.exceptionFailures = 0;
 		this.totalFailures = this.result.getFailureCount();
-		//this.ignoredTests = this.result.getIgnoreCount();
-		this.totalTests = this.result.getRunCount();// + this.ignoredTests;
+		this.totalTests = this.result.getRunCount();
 		List<Failure> failures = this.result.getFailures();
 		for (Failure f : failures) {
-			if (f.getException() != null && f.getException().getClass().getCanonicalName().compareTo("java.lang.Exception") == 0 && (f.getException().getMessage() != null && f.getException().getMessage().startsWith("test timed out"))) {
+			if (isTimeoutFailure(f)) {
 				this.timeoutFailures++;
 			} else if (f.getException() != null && f.getException() instanceof AssertionError) {
 				this.assertFailingTests++;
@@ -34,6 +47,14 @@ public class TestResult {
 			}
 		}
 		this.passingTests = this.totalTests - this.totalFailures;
+	}
+	
+	private boolean isTimeoutFailure(Failure f) {
+		if (f.getException() == null) return false; 
+		if (f.getException().getClass().getCanonicalName().compareTo("java.lang.Exception") != 0) return false;
+		if (f.getException().getMessage() == null) return false;
+		if (f.getException().getMessage().contains("test timed out")) return true;
+		return false;
 	}
 	
 	public long getRunTime() {
@@ -55,10 +76,6 @@ public class TestResult {
 	public int passingTests() {
 		return this.passingTests;
 	}
-	
-//	public int getIgnoredTests() {
-//		return this.ignoredTests;
-//	}
 	
 	public int getTimedoutTests() {
 		return this.timeoutFailures;
@@ -82,8 +99,7 @@ public class TestResult {
 		rep += "Test class runned       : " + this.testClassRunned.getName() + "\n";
 		rep += "Total tests             : " + this.totalTests + "\n";
 		rep += "Passing tests           : " + this.passingTests + "\n";
-		rep += "Runned tests            : " + this.getRunnedTestsCount() + "\n";
-		//rep += "Ignored tests : " + this.ignoredTests + "\n";
+		rep += "Runned tests            : " + this.result.getRunCount() + "\n";
 		rep += "Failing tests           : " + this.totalFailures + "\n";
 		rep += "Timed out tests         : " + this.timeoutFailures + "\n";
 		rep += "Exception failing tests : " + this.exceptionFailures + "\n";
