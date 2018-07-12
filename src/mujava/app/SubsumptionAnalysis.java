148024ec1947f7e5be581df69433a3826e4f0459
Simon Emmanuel Gutierrez Brida <simon.gutierrez.brida@gmail.com>
2018-07-12 20:24:56 -0300

    Implemented subsumption analysis

---
+++ src/mujava/app/SubsumptionAnalysis.java
@@ -0,0 +1,250 @@
+package mujava.app;
+
+import java.io.File;
+import java.io.IOException;
+import java.lang.ProcessBuilder.Redirect;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.Collections;
+import java.util.Iterator;
+import java.util.LinkedList;
+import java.util.List;
+
+public class SubsumptionAnalysis {
+	
+	public static boolean fullPrint = false;
+	private List<SubsumptionNode> nodes = new LinkedList<>();
+	private static enum AnalisisType {EQUIVALENT, SUBSUMING};
+	private boolean analyzed = false;
+	
+	public synchronized void add(SubsumptionNode snode) {
+		for (SubsumptionNode o : nodes) {
+			if (o == snode) throw new IllegalArgumentException("mujava.app.SubsumptionAnalysis#add(SubsumptionNode) : repeated node");
+			if (o.getMutantsID().compareTo(snode.getMutantsID()) == 0) {
+				o.merge(snode);
+				return;
+			}
+		}
+		nodes.add(snode);
+	}
+	
+	public void analyse() {
+		if (analyzed) return;
+		analize(AnalisisType.EQUIVALENT);
+		analize(AnalisisType.SUBSUMING);
+		analyzed = true;
+	}
+	
+	private void analize(AnalisisType at) {
+		boolean merged = false;
+		do {
+			merged = false;
+			for (int i = 0; i < nodes.size(); i++) {
+				SubsumptionNode node = nodes.get(i);
+				for (int j = (i+1); j < nodes.size(); j++) {
+					SubsumptionNode onode = nodes.get(j);
+					switch (at) {
+						case EQUIVALENT: {
+							if (node.mergeIfEquivalent(onode)) {
+								merged = true;
+								nodes.remove(j);
+							}
+							break;
+						}
+						case SUBSUMING: {
+							if (node.subsume(onode)) {
+								node.addSubsumedNode(onode);
+								onode.addSubsumingdNode(node);
+							} else if (onode.subsume(node)) {
+								node.addSubsumingdNode(onode);
+								onode.addSubsumedNode(node);
+							}
+							break;
+						}
+					}
+					
+				}
+				if (merged) break;
+			}
+		} while (merged);
+	}
+	
+	public List<SubsumptionNode> getDominatorNodes() {
+		List<SubsumptionNode> dominators = new LinkedList<>();
+		for (SubsumptionNode n : nodes) {
+			if (n.isDominator()) dominators.add(n);
+		}
+		return dominators;
+	}
+	
+	public List<SubsumptionNode> getNonDominatorNodes() {
+		List<SubsumptionNode> nondominators = new LinkedList<>();
+		for (SubsumptionNode n : nodes) {
+			if (!n.isDominator()) nondominators.add(n);
+		}
+		return nondominators;
+	}
+	
+	@Override
+	public String toString() {
+		StringBuilder sb = new StringBuilder("");
+		Collections.sort(nodes);
+		sb.append("Dynamic Subsumption Analysis\n");
+		SubsumptionNode.fullPrint = fullPrint;
+		Iterator<SubsumptionNode> it = nodes.iterator();
+		while (it.hasNext()) {
+			sb.append(it.next().toString());
+			if (it.hasNext()) sb.append("\n-------\n");
+		}
+		return sb.toString();
+	}
+	
+	public boolean generateDotFile(String file) {
+		Path pfile = Paths.get(file);
+		if (Files.exists(pfile)) {
+			System.err.println(pfile.toString() + " already exists");
+			return true;
+		}
+		Path parent = pfile.toAbsolutePath().getParent();
+		try {
+			Files.createDirectories(parent);
+		} catch (IOException e1) {
+			// TODO Auto-generated catch block
+			e1.printStackTrace();
+			return false;
+		}
+		try {
+			pfile = Files.createFile(pfile);
+		} catch (IOException e) {
+			e.printStackTrace();
+			return false;
+		}
+		analyse();
+		String indent = "\t\t";
+		StringBuilder sb = new StringBuilder();
+		sb.append("digraph dynamic_subsumption_graph {\n");
+		sb.append(indent).append("rankdir=TD;\n");
+		sb.append(indent).append("page=\"15,15\"\n");
+		sb.append(indent).append("ratio=\"auto\";\n");
+		sb.append(indent).append("ranksep=\"1.5 equally\";\n");
+		sb.append(indent).append("node [shape = doubleoctagon fontsize = 30];");
+		Iterator<SubsumptionNode> it = getDominatorNodes().iterator();
+		boolean dominatorsFound = false;
+		while (it.hasNext()) {
+			sb.append(it.next().shortNodeName());
+			if (it.hasNext()) sb.append(" ");
+			dominatorsFound = true;
+		}
+		if (dominatorsFound) sb.append(";");
+		sb.append("\n");
+		sb.append(indent).append("node [shape = rectangle fontsize = 30];\n");
+		it = nodes.iterator();
+		while (it.hasNext()) {
+			SubsumptionNode n = it.next();
+			if (n.isEquivalentToOriginal() || (n.getSubsumedNodes().isEmpty() && n.getSubsumingNodes().isEmpty())) continue;
+			Path nref = saveNodeToFile(parent, n);
+			if (nref == null) return false;
+			sb.append(indent)
+				.append(n.shortNodeName())
+				.append("[").append("URL=\"file:").append(nref.toString()).append("\"];\n");
+			if (!n.getSubsumedNodes().isEmpty()) {
+				Iterator<SubsumptionNode> itsubsumed = n.getSubsumedNodes().iterator();
+				while (itsubsumed.hasNext()) {
+					SubsumptionNode nsub = itsubsumed.next();
+//					URL nsref = saveNodeToFile(parent, nsub);
+//					if (nsref == null) return;
+					sb.append(indent)
+						.append(n.shortNodeName())
+//						.append("[\"").append("HREF=").append(nref.toString()).append("\"]")
+							.append(" -> ")
+						.append(nsub.shortNodeName())
+//						.append("[\"").append("HREF=").append(nsref.toString()).append("\"]")
+					.append(";\n");
+				}
+			}
+		}
+		sb.append("}\n");
+		try {
+			Files.write(pfile, sb.toString().getBytes());
+			return true;
+		} catch (IOException e) {
+			e.printStackTrace();
+			return false;
+		}
+	}
+	
+	public boolean generateSVG(String file, boolean reduceDynamicSubsumptionGraph) {
+		Path pfile = Paths.get(file.replace(".dot", ".svg"));
+		if (Files.exists(pfile)) {
+			System.err.println(pfile.toString() + " already exists");
+			return true;
+		}
+		//dot -Tsvg -odsg.svg dsg.dot
+		String[] args = reduceDynamicSubsumptionGraph?getReducedGraphGenerationCommand(file):getGraphGenerationCommand(file);
+		ProcessBuilder pb = new ProcessBuilder(args);
+		File errorLog = new File("error.log");
+		pb.redirectError(Redirect.appendTo(errorLog));
+		Process p;
+		try {
+			p = pb.start();
+			int exitCode = p.waitFor();
+			if (exitCode != 0) {
+				System.err.println("Failed to generate svg file");
+				return false;
+			} else {
+				return true;
+			}
+		} catch (IOException | InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+			return false;
+		}
+	}
+	
+	private String[] getGraphGenerationCommand(String file) {
+		String[] args = new String[4];
+		args[0] = "dot";
+		args[1] = "-Tsvg";
+		args[2] = "-o"+file.replace(".dot", ".svg");
+		args[3] = file;
+		return args;
+	}
+	
+	private String[] getReducedGraphGenerationCommand(String file) {
+		//String[] cmd = { "/bin/sh", "-c", "ps -ef | grep export" };
+		//tred test.dot | dot -T png > test.png
+		String[] args = new String[3];
+		args[0] = "/bin/sh";
+		args[1] = "-c";
+		args[2] = "tred " + file + " | dot -Tsvg -o " + file.replace(".dot", ".svg");
+		return args;
+	}
+	
+	private Path saveNodeToFile(Path parent, SubsumptionNode n) {
+		String nodeName = n.shortNodeName();
+		String fileName = nodeName + ".txt";
+		Path pfile = Paths.get(parent.toString(), fileName);
+		if (Files.exists(pfile)) {
+			return pfile.getFileName();
+		}
+		try {
+			pfile = Files.createFile(pfile);
+		} catch (IOException e) {
+			e.printStackTrace();
+			return null;
+		}
+		try {
+			boolean fullPrintBackup = SubsumptionNode.fullPrint;
+			SubsumptionNode.fullPrint = true;
+			String data = n.toString();
+			SubsumptionNode.fullPrint = fullPrintBackup;
+			Files.write(pfile, data.getBytes());
+		} catch (IOException e) {
+			e.printStackTrace();
+			return null;
+		}
+		return pfile.getFileName();
+	}
+
+}
---
+++ src/mujava/app/SubsumptionNode.java
@@ -0,0 +1,276 @@
+package mujava.app;
+
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Iterator;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Map;
+import java.util.Map.Entry;
+import java.util.Set;
+import java.util.TreeMap;
+
+import mujava.api.MutationOperator;
+import mujava.util.JustCodeDigest;
+
+public class SubsumptionNode implements Comparable<SubsumptionNode>{
+	
+	public static boolean fullPrint = false;
+	private Map<String, boolean[]> failedTestsPerClass;
+	private final String ID;
+	private List<MutantInfo> mutants;
+	private Set<SubsumptionNode> subsumedNodes;
+	private Set<SubsumptionNode> subsumingNodes;
+	private Map<String,Integer> failingTestsCountPerClass = new TreeMap<>();
+	private boolean hasAtleastOneFailingTests = false;
+	
+	public SubsumptionNode(MutantInfo mi, Map<String, boolean[]> failedTestsPerClass) {
+		mutants = new LinkedList<>();
+		subsumedNodes = new HashSet<>();
+		subsumingNodes = new HashSet<>();
+		mutants.add(mi);
+		StringBuilder sb = new StringBuilder("");
+		Iterator<MutationOperator> it = mi.getMutantOperators().iterator();
+		while (it.hasNext()) {
+			sb.append(it.next().name());
+			if (it.hasNext()) sb.append("_");
+		}
+		ID = sb.toString();
+		this.failedTestsPerClass = failedTestsPerClass;
+		for (Entry<String, boolean[]> entry : failedTestsPerClass.entrySet()) {
+			int count = 0;
+			for (boolean b : entry.getValue()) if (b) {
+				count++;
+				hasAtleastOneFailingTests = true;
+			}
+			failingTestsCountPerClass.put(entry.getKey(), count);
+		}
+	}
+	
+	public boolean hasFailingTests() {
+		return hasAtleastOneFailingTests;
+	}
+	
+	public boolean subsume(SubsumptionNode other) {
+		if (other == null) {
+			throw new IllegalArgumentException("mujava.app.SubsumptionNode#subsume(SubsumptionNode) : null argument");
+		}
+		if (!hasFailingTests() || !other.hasFailingTests()) return false;
+		for (String testClass : failedTestsPerClass.keySet()) {
+			if (failingTestsCountPerClass.containsKey(testClass) && failingTestsCountPerClass.get(testClass) > 0) {
+				if (!other.failingTestsCountPerClass.containsKey(testClass)) return false;
+				if (other.failingTestsCountPerClass.get(testClass) == 0) return false;
+				if (!testClassResultsSubsumed(other, testClass)) return false;
+			}
+		}
+		return true;
+	}
+	
+	private boolean testClassResultsSubsumed(SubsumptionNode other, String testClass) {
+		boolean[] failedTestsForClass = failedTestsPerClass.get(testClass);
+		boolean[] ofailedTestsForClass = other.failedTestsPerClass.get(testClass);
+		if (failedTestsForClass.length != ofailedTestsForClass.length) {
+			throw new IllegalArgumentException("mujava.app.SubsumptionNode#testClassResultsSubsumed(SubsumptionNode, String) : nodes have different amount of tests for test class " + testClass);
+		}
+		for (int i = 0; i < failedTestsForClass.length; i++) {
+			if (failedTestsForClass[i]) {
+				if (!ofailedTestsForClass[i]) return false;
+			}
+		}
+		return true;
+	}
+	
+	public boolean equivalent(SubsumptionNode other) {
+		return subsume(other) && other.subsume(this);
+	}
+	
+	public boolean isDominatorWithRespectTo(List<SubsumptionNode> others) {
+		if (others == null || others.isEmpty()) throw new IllegalArgumentException("mujava.app.SubsumptionNode#isDominator(List<SubsumptionNode>) : list can't be empty or null");
+		boolean subsumesAtLeastOne = false;
+		for (SubsumptionNode o : others) {
+			if (this == o) continue;
+			if (o.subsume(this)) return false;
+			if (!subsumesAtLeastOne && subsume(o)) {
+				subsumesAtLeastOne = true;
+			}
+		}
+		return subsumesAtLeastOne;
+	}
+	
+	public boolean isDominator() {
+		return subsumingNodes.isEmpty() && !subsumedNodes.isEmpty();
+	}
+	
+	public boolean mergeIfEquivalent(SubsumptionNode other) {
+		if (equivalent(other)) {
+			mutants.addAll(other.mutants);
+			return true;
+		}
+		return false;
+	}
+	
+	public void merge(SubsumptionNode other) {
+		for (Entry<String, boolean[]> oentry : other.failedTestsPerClass.entrySet()) {
+			String testClass = oentry.getKey();
+			if (!failedTestsPerClass.containsKey(testClass)) {
+				failedTestsPerClass.put(testClass, oentry.getValue());
+				int ocount = other.failingTestsCountPerClass.get(testClass);
+				failingTestsCountPerClass.put(testClass, ocount);
+				if (!hasAtleastOneFailingTests && ocount > 0) hasAtleastOneFailingTests = true;
+			} else {
+				boolean[] failedTests = failedTestsPerClass.get(testClass);
+				boolean[] ofailedTests = other.failedTestsPerClass.get(testClass);
+				if (failedTests.length != ofailedTests.length) {
+					throw new IllegalArgumentException("mujava.app.SubsumptionNode#merge(SubsumptionNode) : nodes have different amount of tests for test class " + testClass);
+				}
+				int currentCount = failingTestsCountPerClass.get(testClass);
+				for (int i = 0; i < failedTests.length; i++) {
+					if (!failedTests[i] && ofailedTests[i]) {
+						currentCount++;
+						failedTests[i] = true;
+					}
+				}
+				hasAtleastOneFailingTests = true;
+				failingTestsCountPerClass.put(testClass, currentCount);
+			}
+		}
+	}
+	
+//	public boolean analyzeAndUpdate(SubsumptionNode other) {
+//		if (!mergeIfEquivalent(other)) {
+//			if (subsume(other)) {
+//				subsumedNodes.add(other);
+//				other.subsumingNodes.add(this);
+//			} else if (other.subsume(this)) {
+//				subsumingNodes.add(other);
+//				other.subsumedNodes.add(this);
+//			}
+//			return false;
+//		} else {
+//			return true;
+//		}
+//	}
+	
+	public void addSubsumedNode(SubsumptionNode n) {
+		subsumedNodes.add(n);
+	}
+	
+	public void addSubsumingdNode(SubsumptionNode n) {
+		subsumingNodes.add(n);
+	}
+	
+	public Set<SubsumptionNode> getSubsumedNodes() {
+		return subsumedNodes;
+	}
+	
+	public Set<SubsumptionNode> getSubsumingNodes() {
+		return subsumingNodes;
+	}
+	
+	public boolean isEquivalentToOriginal() {
+		return subsumedNodes.isEmpty() && subsumingNodes.isEmpty();
+	}
+	
+	public String getMutantsID() {
+		StringBuilder sb = new StringBuilder();
+		Iterator<MutantInfo> it = mutants.iterator();
+		while (it.hasNext()) {
+			MutantInfo mi = it.next();
+			sb.append(Arrays.toString(mi.getMD5digest()));
+			if (it.hasNext()) sb.append(".");
+		}
+		return sb.toString();
+	}
+	
+	@Override
+	public boolean equals(Object o) {
+		if (o == null) return false;
+		if (o instanceof SubsumptionNode) {
+			SubsumptionNode other = (SubsumptionNode) o;
+			String[] hashes = getMutantsID().split(".");
+			String omid = other.getMutantsID();
+			for (String h : hashes) {
+				omid.replace(h, "");
+			}
+			return omid.trim().isEmpty();
+		}
+		return false;
+	}
+	
+	public String nodeName() {
+		String hash =JustCodeDigest.toHex(JustCodeDigest.digest(getMutantsID(), false));
+		StringBuilder sb = new StringBuilder();
+		sb.append(ID).append("_").append(mutants.size()).append("_").append(hash);
+		return sb.toString();
+	}
+	
+	public String shortNodeName() {
+		String hash =JustCodeDigest.toHex(JustCodeDigest.digest(getMutantsID(), false));
+		StringBuilder sb = new StringBuilder();
+		sb.append(ID).append("_").append(mutants.size()).append("_").append(hash.substring(0, 7));
+		return sb.toString();
+	}
+	
+	@Override
+	public String toString() {
+		StringBuilder sb = new StringBuilder(nodeName());
+		if (isDominator()) sb.append("-DOMINATOR");
+		if (SubsumptionNode.fullPrint) {
+			sb.append("\n");
+			Iterator<Entry<String, boolean[]>> it = failedTestsPerClass.entrySet().iterator();
+			while (it.hasNext()) {
+				Entry<String, boolean[]> curr = it.next();
+				sb.append("Test class : ").append(curr.getKey()).append(Arrays.toString(curr.getValue()));
+				sb.append("\n");
+			}
+			Iterator<MutantInfo> mit = mutants.iterator();
+			while (mit.hasNext()) {
+				sb.append(mit.next().toString());
+				if (mit.hasNext()) sb.append("\n");
+			}
+			
+		}
+		if (SubsumptionNode.fullPrint) {
+			boolean fullPrintBackup = SubsumptionNode.fullPrint;
+			SubsumptionNode.fullPrint = false;
+			sb.append("\n");
+			if (!isDominator() &&  !subsumingNodes.isEmpty()) {
+				sb.append("Subsuming nodes: \n");
+				for (SubsumptionNode subsuming : subsumingNodes) {
+					sb.append(subsuming.toString()).append("\n");
+				}
+			}
+			if (!subsumedNodes.isEmpty()) {
+				sb.append("Subsumed nodes: \n");
+				for (SubsumptionNode subsumed : subsumedNodes) {
+					sb.append(subsumed.toString()).append("\n");
+				}
+			}
+			SubsumptionNode.fullPrint = fullPrintBackup;
+		}
+		return sb.toString();
+	}
+
+	@Override
+	public int compareTo(SubsumptionNode o) {
+		if (this == o) return 0;
+		int failingTests = 0;
+		int ofailingTests = 0;
+		for (Entry<String, Integer> entry : failingTestsCountPerClass.entrySet()) {
+			int fcount = o.failingTestsCountPerClass.get(entry.getKey());
+			if (o.failingTestsCountPerClass.containsKey(entry.getKey())) {
+				failingTests += (fcount - o.failingTestsCountPerClass.get(entry.getKey()));
+			} else {
+				failingTests += fcount;
+			}
+		}
+		int subsumed = subsumedNodes.size();
+		int subsuming = subsumingNodes.size();
+		int osubsumed = o.subsumedNodes.size();
+		int osubsuming = o.subsumingNodes.size();
+		int value = failingTests + (subsumed - subsuming);
+		int ovalue = ofailingTests + (osubsumed - osubsuming);
+		return value - ovalue;
+	}
+	
+}
