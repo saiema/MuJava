a7aed7de1ca3a2f2ddb93c104f8f201d0d276810
Simon Emmanuel Gutierrez Brida <simon.gutierrez.brida@gmail.com>
2018-07-12 16:32:23 -0300

    added a #toHex(byte[]) to output a friendly representation

--- src/mujava/util/JustCodeDigest.java
+++ src/mujava/util/JustCodeDigest.java
@@ -83,6 +83,18 @@ public class JustCodeDigest {
 		return null;
 	}
 	
+	public static String toHex(byte[] bytes) {
+		char[] hexArray = {'0','1','2','3','4','5','6','7','8','9','A','B','C','D','E','F'};
+	    char[] hexChars = new char[bytes.length * 2];
+	    int v;
+	    for ( int j = 0; j < bytes.length; j++ ) {
+	        v = bytes[j] & 0xFF;
+	        hexChars[j*2] = hexArray[v/16];
+	        hexChars[j*2 + 1] = hexArray[v%16];
+	    }
+	    return new String(hexChars);
+    }
+	
 	public static byte[] digest(File file) {
 		return digest(file, true);
 	}
