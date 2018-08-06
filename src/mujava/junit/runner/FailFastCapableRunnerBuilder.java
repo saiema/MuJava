a9dd0ccb7fc6aa87edf873e2594d67a03a2aca7b
Simon Emmanuel Gutierrez Brida <simon.gutierrez.brida@gmail.com>
2018-08-06 19:20:54 -0300

    Added verbose mode (for debugging), enabled by setting MuJavaJunitTestRunnerBuilder.verbose to true

--- src/mujava/junit/runner/FailFastCapableRunnerBuilder.java
+++ src/mujava/junit/runner/FailFastCapableRunnerBuilder.java
@@ -18,6 +18,7 @@ public class FailFastCapableRunnerBuilder extends RunnerBuilder {
 	
 	private boolean failFast;
 	private long timeout;
+	private boolean verbose = MuJavaJunitTestRunnerBuilder.verbose;
 	
 	public FailFastCapableRunnerBuilder(boolean failFast, long timeout) {
 		this.failFast = failFast;
@@ -30,20 +31,27 @@ public class FailFastCapableRunnerBuilder extends RunnerBuilder {
 	}
 	
 	private ParentRunner<?> retrieveTestRunner(Class<?> testToRun) throws Throwable {
+		if (verbose) System.out.println("mujava.junit.runner.FailFastCapableRunnerBuilder: retrieving runner for class " + testToRun.getName());
 		RunWith runWithAnnotation = testToRun.getAnnotation(RunWith.class);
 		if (runWithAnnotation != null) {
+			if (verbose) System.out.println("runWith annotation found");
 			if (runWithAnnotation.value().equals(Parameterized.class)) {
+				if (verbose) System.out.println("runWith Parameterized.class");
 				return new FailFastCapableParameterized(testToRun, failFast, timeout);
 			} else if (runWithAnnotation.value().equals(Suite.class)) {
+				if (verbose) System.out.println("runWith Suite.class");
 				return new Suite(testToRun, this);
 			}
 		} else if (TestCase.class.isAssignableFrom(testToRun)) {
+			if (verbose) System.out.println("Test class extends TestCase");
 			FailFastCapableBlockJUnit4ClassRunner.ignore = false;
 			return new FailFastCapableBlockJUnit4ClassRunner(testToRun, failFast, timeout);
 		} else if (TestSuite.class.isAssignableFrom(testToRun)) {
+			if (verbose) System.out.println("Test class extends TestSuite");
 			FailFastCapableBlockJUnit4ClassRunner.ignore = false;
 			return new FailFastCapableBlockJUnit4ClassRunner(testToRun, failFast, timeout);
 		} else if (testToRun.getAnnotation(Test.class) != null) {
+			if (verbose) System.out.println("Test annotation found in test class");
 			FailFastCapableBlockJUnit4ClassRunner.ignore = false;
 			return new FailFastCapableBlockJUnit4ClassRunner(testToRun, failFast, timeout);
 		} else {
@@ -55,11 +63,13 @@ public class FailFastCapableRunnerBuilder extends RunnerBuilder {
 				}
 			}
 			if (hasAtLeastOneTest) {
+				if (verbose) System.out.println("At least one test method found");
 				FailFastCapableBlockJUnit4ClassRunner.ignore = false;
 				return new FailFastCapableBlockJUnit4ClassRunner(testToRun, failFast, timeout);
 			}
 			throw new IllegalArgumentException("Class : " + testToRun.toString() + " is not a valid junit test");
 		}
+		if (verbose) System.out.println("No conditions met");
 		FailFastCapableBlockJUnit4ClassRunner.ignore = false;
 		return new FailFastCapableBlockJUnit4ClassRunner(testToRun, false, timeout);
 	}
