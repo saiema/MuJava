f1c17d27d31a4c385d88144b4093c77149af5fbc
Simon Emmanuel Gutierrez Brida <simon.gutierrez.brida@gmail.com>
2019-03-13 15:29:02 -0300

    Missing class

---
+++ src/mujava/junit/runner/DiscardedException.java
@@ -0,0 +1,14 @@
+package mujava.junit.runner;
+
+public class DiscardedException extends Throwable {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	public DiscardedException(String message) {
+		super(message);
+	}
+
+}
